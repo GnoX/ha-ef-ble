7b6b46247eec089727c77cfd5995814d79112b39
GnoX <dev@gnox.dev>
2025-03-08 21:48:20 +0100

    Fix config flow and translations, add ecoflow login

--- custom_components/ef_ble/eflib/devicebase.py
+++ custom_components/ef_ble/eflib/devicebase.py
@@ -1,9 +1,9 @@
-import asyncio
 import logging
 from collections.abc import Callable
 
 from bleak.backends.device import BLEDevice
 from bleak.backends.scanner import AdvertisementData
+from bleak_retry_connector import MAX_CONNECT_ATTEMPTS
 
 from .connection import Connection
 from .packet import Packet
@@ -19,6 +19,7 @@ class DeviceBase:
     def __init__(
         self, ble_dev: BLEDevice, adv_data: AdvertisementData, sn: str
     ) -> None:
+        self._sn = sn
         _LOGGER.debug(
             "%s: Creating new device: %s (%s)",
             ble_dev.address,
@@ -27,14 +28,12 @@ class DeviceBase:
         )
         self._ble_dev = ble_dev
         self._address = ble_dev.address
-        self._sn = sn
         # We can't use advertisement name here - it's prone to change to "Ecoflow-dev"
         self._name = self.NAME_PREFIX + self._sn[-4:]
         self._name_by_user = self._name
 
         self._conn = None
         self._callbacks = set()
-        self._callbacks_map = dict()
 
     @property
     def device(self):
@@ -59,6 +58,10 @@ class DeviceBase:
     def is_connected(self) -> bool:
         return self._conn != None and self._conn.is_connected
 
+    @property
+    def connection_state(self):
+        return None if self._conn is None else self._conn._state
+
     async def data_parse(self, packet: Packet):
         """Function to parse incoming data and trigger sensors update"""
         return False
@@ -67,19 +70,22 @@ class DeviceBase:
         """Function to parse packet"""
         return Packet.fromBytes(data)
 
-    async def connect(self, user_id: str | None = None):
-        if self._conn == None:
-            if user_id != None:
-                self._user_id = user_id
+    async def connect(
+        self, user_id: str | None = None, max_attempts: int = MAX_CONNECT_ATTEMPTS
+    ):
+        if self._conn is None:
             self._conn = Connection(
                 self._ble_dev,
                 self._sn,
-                self._user_id,
+                user_id,
                 self.data_parse,
                 self.packet_parse,
             )
             _LOGGER.info("%s: Connecting to %s", self._address, self.__doc__)
-        await self._conn.connect()
+        elif self._conn._user_id != user_id:
+            self._conn._user_id = user_id
+
+        await self._conn.connect(max_attempts=max_attempts)
 
     async def disconnect(self):
         if self._conn == None:
@@ -92,15 +98,16 @@ class DeviceBase:
         if self._conn == None:
             _LOGGER.error("%s: Device has no connection", self._address)
             return
-
-        await self._conn.waitConnected()
+        if not self.is_connected:
+            await self._conn.waitConnected()
 
     async def waitDisconnected(self):
         if self._conn == None:
             _LOGGER.error("%s: Device has no connection", self._address)
             return
 
-        await self._conn.waitDisconnected()
+        if self.is_connected:
+            await self._conn.waitDisconnected()
 
     def register_callback(
         self, callback: Callable[[], None], propname: str | None = None
@@ -123,6 +130,6 @@ class DeviceBase:
             self._callbacks_map.get(propname, set()).discard(callback)
 
     def update_callback(self, propname: str) -> None:
-        """Finding the registered callbacks in the map and then calling the callbacks"""
+        """Find the registered callbacks in the map and then calling the callbacks"""
         for callback in self._callbacks_map.get(propname, set()):
             callback()
