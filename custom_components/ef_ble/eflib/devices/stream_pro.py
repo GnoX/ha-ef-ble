0685d703c7f340c8375afd1ed76e28e76d625108
GnoX <dev@gnox.dev>
2025-07-19 21:38:57 +0200

    Add initial support for stream devices

---
+++ custom_components/ef_ble/eflib/devices/stream_pro.py
@@ -0,0 +1,13 @@
+from ..props import ProtobufProps, pb_field
+from . import stream_ac, stream_max
+
+pb = stream_ac.pb
+
+
+class Device(stream_max.Device, ProtobufProps):
+    """STREAM Pro"""
+
+    SN_PREFIX = (b"BK12",)
+
+    ac_power_2 = pb_field(pb.pow_get_schuko2)
+    pv_power_3 = pb_field(pb.pow_get_pv3)
