cb2652ee746d9b3f6c9175d6bc8d54108bb4cf79
GnoX <dev@gnox.dev>
2025-08-23 22:34:46 +0200

    Add rounding for STREAM PV sensors

--- custom_components/ef_ble/eflib/devices/stream_pro.py
+++ custom_components/ef_ble/eflib/devices/stream_pro.py
@@ -12,4 +12,4 @@ class Device(stream_max.Device, ProtobufProps):
     ac_2 = pb_field(pb.relay3_onoff)
     ac_power_2 = pb_field(pb.pow_get_schuko2)
 
-    pv_power_3 = pb_field(pb.pow_get_pv3)
+    pv_power_3 = pb_field(pb.pow_get_pv3, lambda v: round(v, 2))
