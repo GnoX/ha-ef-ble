6e23c8738f9f166328daaba943a4581508c5c9a7
GnoX <gnox@gnox.dev>
2025-08-24 19:42:10 +0200

    Fix energy operation mode assignment for STREAM

--- custom_components/ef_ble/eflib/devices/stream_max.py
+++ custom_components/ef_ble/eflib/devices/stream_max.py
@@ -12,7 +12,6 @@ class Device(stream_ac.Device, ProtobufProps):
     ac_power_1 = pb_field(pb.pow_get_schuko1)
     ac_1 = pb_field(pb.relay2_onoff)
 
-    pv_sum = pb_field(pb.pow_get_pv_sum, lambda v: round(v, 2))
     pv_power_1 = pb_field(pb.pow_get_pv, lambda v: round(v, 2))
     pv_power_2 = pb_field(pb.pow_get_pv2, lambda v: round(v, 2))
 
