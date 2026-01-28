548eefbad875098cd1da8fe201776e8208e0a005
GnoX <gnox@gnox.dev>
2026-01-28 19:21:37 +0100

    Fix D2M ac commands using incorrect dst header

--- custom_components/ef_ble/eflib/devices/delta2.py
+++ custom_components/ef_ble/eflib/devices/delta2.py
@@ -1,21 +1,12 @@
-from _delta2_base import Delta2Base
-
 from ..model import (
-    AllKitDetailData,
-    DirectBmsMDeltaHeartbeatPack,
-    DirectEmsDeltaHeartbeatPack,
-    DirectInvDeltaHeartbeatPack,
     Mr330MpptHeart,
     Mr330PdHeart,
 )
-from ..packet import Packet
 from ..props.raw_data_field import dataclass_attr_mapper, raw_field
+from ._delta2_base import Delta2Base
 
 pb_pd = dataclass_attr_mapper(Mr330PdHeart)
 pb_mppt = dataclass_attr_mapper(Mr330MpptHeart)
-pb_ems = dataclass_attr_mapper(DirectEmsDeltaHeartbeatPack)
-pb_bms = dataclass_attr_mapper(DirectBmsMDeltaHeartbeatPack)
-pb_inv = dataclass_attr_mapper(DirectInvDeltaHeartbeatPack)
 
 
 class Device(Delta2Base):
@@ -26,115 +17,24 @@ class Device(Delta2Base):
 
     ac_input_power = raw_field(pb_pd.ac_input_watts)
     plugged_in_ac = raw_field(pb_pd.ac_charge_flag, lambda x: x == 1)
-
-    battery_level = raw_field(pb_bms.f32_show_soc, lambda x: round(x, 2))
-    input_power = raw_field(pb_pd.watts_in_sum)
-    output_power = raw_field(pb_pd.watts_out_sum)
-
-    usbc_output_power = raw_field(pb_pd.typec1_watts)
-    usba_output_power = raw_field(pb_pd.usb1_watt)
-
-    usb_ports = raw_field(pb_pd.dc_out_state, lambda x: x == 1)
-
-    battery_charge_limit_min = raw_field(pb_ems.min_dsg_soc)
-    battery_charge_limit_max = raw_field(pb_ems.max_charge_soc)
-
-    cell_temperature = raw_field(pb_pd.car_temp)
-
-    dc_12v_port = raw_field(pb_pd.car_state, lambda x: x == 1)
+    energy_backup_battery_level = raw_field(pb_pd.bp_power_soc)
     dc_output_power = raw_field(pb_pd.dc_pv_output_watts)
+    ac_charging_speed = raw_field(pb_mppt.cfg_chg_watts)
 
-    ac_ports = raw_field(pb_pd.cfg_ac_enabled, lambda x: x == 1)
+    @property
+    def pd_heart_type(self):
+        return Mr330PdHeart
+
+    @property
+    def mppt_heart_type(self):
+        return Mr330MpptHeart
 
     def __init__(self, ble_dev, adv_data, sn: str) -> None:
         super().__init__(ble_dev, adv_data, sn)
-        self._product_type: int | None = None
+        self.max_ac_charging_power = 1200
 
-    @classmethod
-    def check(cls, sn):
-        return sn[:4] in cls.SN_PREFIX
-
-    @property
-    def device(self):
-        model = "2"
-        match self._sn[:4]:
-            case "D361":
-                model = "3 1500"
-            case "R351" | "R354":
-                model = "2 Max"
-
-        return f"Delta {model}"
-
-    async def packet_parse(self, data: bytes) -> Packet:
-        return Packet.fromBytes(data, is_xor=True)
-
-    async def data_parse(self, packet: Packet) -> bool:
-        """Process the incoming notifications from the device"""
-
-        processed = False
-        self.reset_updated()
-
-        match packet.src, packet.cmdSet, packet.cmdId:
-            case 0x02, 0x20, 0x02:
-                self.update_from_bytes(Mr330PdHeart, packet.payload)
-                processed = True
-            case 0x03, 0x03, 0x0E:
-                detail = self.update_from_bytes(AllKitDetailData, packet.payload)
-                self._update_product_type(detail)
-                processed = True
-            case 0x03, 0x20, 0x02:
-                self.update_from_bytes(DirectEmsDeltaHeartbeatPack, packet.payload)
-                processed = True
-            case 0x03, 0x20, 0x32:
-                self.update_from_bytes(DirectBmsMDeltaHeartbeatPack, packet.payload)
-                processed = True
-            case 0x04, _, 0x02:
-                self.update_from_bytes(DirectInvDeltaHeartbeatPack, packet.payload)
-                processed = True
-            case 0x05, 0x20, 0x02:
-                self.update_from_bytes(Mr330MpptHeart, packet.payload)
-                processed = True
-
-        for field_name in self.updated_fields:
-            self.update_callback(field_name)
-            self.update_state(field_name, getattr(self, field_name))
-
-        return processed
-
-    async def set_battery_charge_limit_max(self, limit: int):
-        packet = Packet(0x21, 0x03, 0x20, 0x31, limit.to_bytes(), version=0x02)
-        await self._conn.sendPacket(packet)
-
-    async def set_battery_charge_limit_min(self, limit: int):
-        packet = Packet(0x21, 0x03, 0x20, 0x33, limit.to_bytes(), version=0x02)
-        await self._conn.sendPacket(packet)
-
-    async def enable_usb_ports(self, enabled: bool):
-        packet = Packet(0x21, 0x02, 0x20, 0x22, enabled.to_bytes(), version=0x02)
-        await self._conn.sendPacket(packet)
-
-    async def enable_dc_12v_port(self, enabled: bool):
-        packet = Packet(
-            0x21, self._dc_12v_dst(), 0x20, 0x51, enabled.to_bytes(), version=0x02
-        )
-        await self._conn.sendPacket(packet)
-
-    async def enable_ac_ports(self, enabled: bool):
-        payload = bytes([1 if enabled else 0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF])
-        packet = Packet(0x21, 0x05, 0x20, 0x42, payload, version=0x02)
-        await self._conn.sendPacket(packet)
-
-    def _update_product_type(self, detail: AllKitDetailData) -> None:
-        if not detail.kit_base_info:
-            return
-
-        sn_bytes = self._sn.encode()
-        for kit in detail.kit_base_info:
-            if kit.sn.rstrip(b"\x00") == sn_bytes:
-                self._product_type = kit.product_type
-                return
-
-        self._product_type = detail.kit_base_info[0].product_type
-
-    def _dc_12v_dst(self) -> int:
-        return 0x07 if self._product_type == 82 else 0x05
+    def _after_message_parsed(self):
+        if self.battery_addon:
+            self.max_ac_charging_power = 1500
+        else:
+            self.max_ac_charging_power = 1200
