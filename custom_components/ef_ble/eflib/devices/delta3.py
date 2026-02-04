bfeee379b86b581b1eb51c022df20cbb08f20c3c
GnoX <gnox@gnox.dev>
2026-02-04 15:11:32 +0100

    Add support for Delta 3 Air

--- custom_components/ef_ble/eflib/devices/delta3.py
+++ custom_components/ef_ble/eflib/devices/delta3.py
@@ -1,6 +1,6 @@
 from ..pb import pd335_sys_pb2
 from ..props import pb_field
-from . import delta3_classic
+from . import _delta3_base, delta3_classic
 
 pb = delta3_classic.pb
 
@@ -11,7 +11,7 @@ class Device(delta3_classic.Device):
     SN_PREFIX = (b"P231",)
     NAME_PREFIX = "EF-D3"
 
-    usb_ports = pb_field(pb.flow_info_qcusb1, delta3_classic._flow_is_on)
+    usb_ports = pb_field(pb.flow_info_qcusb1, _delta3_base.flow_is_on)
 
     async def enable_usb_ports(self, enabled: bool):
         await self._send_config_packet(pd335_sys_pb2.ConfigWrite(cfg_usb_open=enabled))
