409180a277deb31dbf39febcb264c679efd25fc4
GnoX <gnox@gnox.dev>
2026-08-09 17:44:08 +0200

    Stop asking the STREAM AC 5000 for a full property upload

--- custom_components/ef_ble/eflib/devices/stream_ac_5000.py
+++ custom_components/ef_ble/eflib/devices/stream_ac_5000.py
@@ -1,6 +1,3 @@
-from bleak.backends.device import BLEDevice
-from bleak.backends.scanner import AdvertisementData
-
 from ..devicebase import DeviceBase
 from ..packet import Packet
 from ..pb import es22_sys_pb2
@@ -16,8 +13,6 @@ class Device(DeviceBase, ProtobufProps):
 
     SN_PREFIX = (b"ES22",)
     NAME_PREFIX = "EF-6"
-
-    _FULL_UPLOAD_INTERVAL = 300
 
     battery_level = pb_field(pb.system.info.bp_soc)
     battery_power = pb_field(
@@ -39,14 +34,6 @@ class Device(DeviceBase, ProtobufProps):
     _bp_temp2 = pb_field(pb.battery.info.bp_temp2)
     _bp_temp3 = pb_field(pb.battery.info.bp_temp3)
     _bp_temp4 = pb_field(pb.battery.info.bp_temp4)
-
-    def __init__(
-        self, ble_dev: BLEDevice, adv_data: AdvertisementData, sn: str
-    ) -> None:
-        super().__init__(ble_dev, adv_data, sn)
-        self.add_timer_task(
-            self._request_full_upload, interval=self._FULL_UPLOAD_INTERVAL
-        )
 
     @classmethod
     def check(cls, sn):
@@ -80,16 +67,3 @@ class Device(DeviceBase, ProtobufProps):
         self._notify_updated()
 
         return processed
-
-    async def _request_full_upload(self) -> None:
-        if self._conn is None:
-            return
-        message = es22_sys_pb2.ConfigWrite(
-            active_display_property_full_upload=True,
-            active_runtime_property_full_upload=True,
-        )
-        await self._conn.sendPacket(
-            Packet(
-                0x21, 0x02, 0xFE, 0x11, message.SerializeToString(), 0x01, 0x01, 0x03
-            )
-        )
